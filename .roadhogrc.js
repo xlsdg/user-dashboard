9cce78c7f39d8b09b5e9476975cf0df8d037e8a1
xLsDg <xlsdg@qq.com>
2017-08-08 16:55:15 +0800

    chore(mod): upgrade modules

--- .roadhogrc.js
+++ .roadhogrc.js
@@ -1,14 +1,14 @@
 export default {
+  hash: true,
   entry: 'src/index.js',
-  multipage: true,
-  publicPath: '/',
   disableCSSModules: false,
   autoprefixer: {
     browsers: [
-      'last 7 versions', 'Android >= 4.2', 'iOS >= 6'
+      'last 7 versions',
+      'Android >= 4.2',
+      'iOS >= 6'
     ]
   },
-  proxy: null,
   extraBabelPlugins: [
     'transform-runtime',
     ['import', {
@@ -18,9 +18,12 @@ export default {
   ],
   env: {
     production: {
+      multipage: true,
       publicPath: '/user-dashboard/'
     },
     development: {
+      multipage: false,
+      publicPath: '/',
       extraBabelPlugins: [
         'dva-hmr'
       ]
